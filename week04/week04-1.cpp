5bf0e9552299126ce61d4afbea206634f997be17
13750404 <yixiangceng74@gmail.com>
2025-10-02 17:36:25 +0800

    week04

---
+++ week04/week04-1.cpp
@@ -0,0 +1,14 @@
+//week04-1.cpp 家览肈,酚帝肈ヘ糶祘Α
+//LeetCode 珼驹肈 3100. Water Bottles II
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans=numBottles;//秨﹍碞耻硂或瞺
+        while(numBottles >=numExchange) {// 狦繵计 >=传秖,碞传
+            numBottles = numBottles - numExchange + 1;//传1繵都
+            ans++;//耻瞺
+            numExchange ++;//传夹非 瞺
+    }
+    return ans;
+    }
+};
